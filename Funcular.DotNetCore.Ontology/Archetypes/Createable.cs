fd33430daefed7f972bf124b6b402c80115f923a
piranout <ps+github@ps.cm>
2017-06-27 15:00:39 -0500

    Initial commit
    Implemented INotifyPropertyChanged, made DateCreatedUtc and DateModifiedUtc non-nullable

---
+++ Funcular.DotNetCore.Ontology/Archetypes/Createable.cs
@@ -0,0 +1,134 @@
+﻿#region File info
+// *********************************************************************************************************
+// Funcular.Ontology>Funcular.Ontology>Createable.cs
+// Created: 2015-07-01 2:16 PM
+// Updated: 2015-07-01 2:50 PM
+// By: Paul Smith 
+// 
+// *********************************************************************************************************
+// LICENSE: The MIT License (MIT)
+// *********************************************************************************************************
+// Copyright (c) 2010-2015 <copyright holders>
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// *********************************************************************************************************
+#endregion
+
+
+#region Usings
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Funcular.DotNetCore.Ontology.Annotations;
+
+#endregion
+
+
+namespace Funcular.DotNetCore.Ontology.Archetypes
+{
+    public abstract class Createable<TId> : ICreateable<TId>
+    {
+        #region Nonpublic fields
+        protected TId _createdBy;
+        protected TId _id;
+        protected DateTime _dateCreatedUtc = DateTime.UtcNow;
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// If you override this constructor, you will need to re-implement
+        /// the identity assignment function.
+        /// </summary>
+        protected Createable()
+        {
+            if (IdentityFunction != null)
+                this._id = IdentityFunction();
+        }
+        #endregion
+
+
+        #region Implementation of IIdentity
+
+        public virtual TId Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+
+        #region Implementation of ICreateable
+
+        public virtual DateTime DateCreatedUtc
+        {
+            get => _dateCreatedUtc;
+            set
+            {
+                _dateCreatedUtc = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public virtual TId CreatedBy
+        {
+            get => this._createdBy;
+            set
+            {
+                this._createdBy = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+
+        #region Identity assignment method
+        /// <summary>
+        ///     Set this once in your app domain and ID assignment is done
+        ///     for all entities.e.g., calling
+        /// </summary>
+        /// <example>
+        /// // Assuming you have an Id generator instance...
+        /// Createable.IdentityFunction = () => _generator.NewId();
+        /// // Make sure this function does not throw exceptions.
+        /// </example>
+        public static Func<TId> IdentityFunction { get; set; }
+        #endregion
+
+
+        #region OnPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+    }
+}
---
+++ Funcular.DotNetCore.Ontology/README.md
@@ -0,0 +1,87 @@
+# Funcular.Ontology
+##### A coherent and convenient set of interfaces and base classes for entity domains. 
+
+* Dispense with boilerplate persistence code. 
+* Apply Id assignment functions to the entire domain.
+* Centralize handling of timestamp and audit field logic. 
+* Add extension methods to like-sets of entities. 
+
+### Important Note
+This package was forked for .NET Core from Funcular.Ontology, because
+Visual Studio doesn't properly support multi-framework targeting in
+the project properties dialog. 
+
+### Examples 
+```csharp
+// Derive entities from one of the base classes in Archetypes.
+// Funcular.Ontology.Archetypes.Createable<TId> is a simple abstract class 
+// with an Id of <TId> (string in this case), a DateCreatedUtc datetime, 
+// and a CreatedBy of <TId> (again, string here). Every other abstract
+// class in Archetypes derives from this:
+	public partial class TransactionItem : Createable<string>
+	{
+		public Decimal ItemAmount { get; set; }
+		public String ItemText { get; set; }
+	}
+	
+	// Statically assign an Id-generating function to the base type.
+	// Here is a horrible example of an Id function:
+	Createable<string>.IdentityFunction = () => Datetime.Now.ToString();
+	// If you're using the Base36 Id generator with dependency injection:
+	Createable<string>.IdentityFunction = () => _base36.NewId();
+```
+
+### Type Dependency Diagram
+This diagram shows the inheritance tree of the Funcular.Ontology.Archetypes namespace:
+
+![Dependency Diagram](/Content/DependencyGraph.png?raw=true "Dependency Graph")
+
+### Leveraging by Funcular.DataProviders
+
+If you use Funcular.DataProviders with Ontology-derived entities, you get some boilerplate behavior taken care of for free.
+* Id assignment is done entirely in Ontology
+* DataProviders takes care of assigning Create dates, users
+* Also, assigns Modify dates and users
+
+```csharp
+// Named, Labeled and Described are abstract classes in Archetypes, with
+// (as you might expect), Name, Label and Description properties. These
+// are cumulative; Labeled derives from Named, and Described from Labeled.
+// These in turn derive from Modifyable, which adds DateModifiedUtc [datetime?]
+// and ModifiedBy [<TId>] to Createable. 
+	public partial class ThingWithADescription : Described<string>
+	{
+	    /* The following properties are inherited from the Archetypes hierarchy: 
+	       string Id { get; set; } // Id generation logic set in static method
+	       datetime DateCreatedUtc { get; set; }
+	       string CreatedBy { get; set; }
+	       DateTime? DateModifiedUtc { get; set; }
+	       string ModifiedBy { get; set; }
+	       string Name { get; set; }
+	       string Label { get; set; }
+	       string Description { get; set; }
+	     */
+	     
+      // You just add properties specific to this class, e.g.:	     
+      public int? HowManyCatsDoesSheHave { get; set; }
+      public bool IsItTuesday { get; set; }
+      public string Nickname { get; set; }
+	}
+	
+      // Tell the provider about the current user's identity; 
+      // this will eventually be replaced with a UserProvider
+      // instance supplied by dependency injection:
+      _dataProvider.SetCurrentUser<string>(currentUserId);
+      
+      // This will assign CreateDateUtc and CreatedBy:
+      _dataProvider.Insert<ThingWithADescription, string>(myThing);
+      
+      // This will assign ModifyDateUtc and ModifiedBy:
+      _dataProvider.Update<ThingWithADescription, string>(myThing);
+      
+```
+
+### Class Diagram
+This class diagram shows the signature of each interface and abstract class in the Funcular.Ontology.Archetypes namespace:
+
+![Class Diagram](/Content/ClassDiagram.png?raw=true "Class Diagram")
